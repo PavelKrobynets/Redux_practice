8c925e1ee0ad83686e926918a01d52cf3b43c213
Pavel <krobynetspavel@gmail.com>
2024-08-14 13:14:32 +0300

    sending props through store

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore as createStore, bindActionCreators } from "redux";
-import * as actions from "./actions";
+import { legacy_createStore as createStore } from "redux";
 import reducer from "./reducer";
+import App from "./components/App";
+import {Provider} from "react-redux"
 import "./index.scss";
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
 
-const update = () => {
-  document.getElementById("counter").textContent = getState().value;
-};
 
-subscribe(update);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+			<Provider store={store}><App/></Provider>
+    </React.StrictMode>
+  );
 
-const { inc, dec, rnd } = bindActionCreators(actions,dispatch);
 
-document.getElementById("inc").addEventListener("click", inc);
-document.getElementById("dec").addEventListener("click", dec);
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <button
-      className="btn btn-warning"
-      onClick={() => dispatch({ type: "INC" })}
-    >
-      INCREMENT
-    </button>
-  </React.StrictMode>
-);
